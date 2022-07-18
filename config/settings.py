d301972e7b236388cedc0b603287f4ff49f4d77d
gusbarron <67945700+gusbarron@users.noreply.github.com>
2022-07-17 21:07:44 -0700

    Competency Report

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
